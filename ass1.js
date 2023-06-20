6ffa37b9c74713864f36245bd56c1c1a11964135
Sanket Rajnor <107184559+Sanketrajnor@users.noreply.github.com>
2023-06-20 19:26:07 +0530

    Add files via upload

---
+++ ass1.js
@@ -0,0 +1,9 @@
+function calculateArea(length, width) {
+    var area = length * width;
+    return area;
+  }
+  var length = 10;
+  var width = 5;
+  var area = calculateArea(length, width);
+  console.log(area); // Output: 50
+    
